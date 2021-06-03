7b03b1fd926ea739d77aa143a37e906c6570814e
Fahadquraishi007 <84074881+Fahadquraishi007@users.noreply.github.com>
2021-06-03 17:12:10 +0530

    Add files via upload

---
+++ Candidate.java
@@ -0,0 +1,14 @@
+package com.te.projects;
+
+public class Candidate {
+	String name;
+	long mob;
+	double per;
+	public Candidate(String name, long mob, double per) {
+		super();
+		this.name = name;
+		this.mob = mob;
+		this.per = per;
+	}
+	
+}
